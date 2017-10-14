cfacfd7aaa8d2918bf0764b9326be7b4df739e1d
Umashankar Mishra <umashankar.mishra@humanebits.com>
2017-10-14 12:39:41 +0530

    deleted unwanted stuff

--- app/src/main/java/com/honap/madhumitra/entity/BpValueType.java
+++
@@ -1,9 +0,0 @@
-package com.honap.madhumitra.entity;
-
-/**
- * Author: Chetan S.
- */
-public class BpValueType {
-    public final static String SYSTOLIC = "systolic";
-    public final static String DIASTOLIC = "diastolic";
-}
