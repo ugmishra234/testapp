1952a72f0c65c47d0a2d20eebb70dc603e91a21a
Umashankar Mishra <umashankar.mishra@humanebits.com>
2017-11-22 17:08:38 +0530

    Completed dashboard UI layout

---
+++ Madhumitra/app/src/main/java/com/honap/madhumitra/fragment/DashBoardFragment.java
@@ -0,0 +1,61 @@
+package com.honap.madhumitra.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.honap.madhumitra.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link DashBoardFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class DashBoardFragment extends Fragment {
+
+    public DashBoardFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment DashBoardFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static DashBoardFragment newInstance(String param1, String param2) {
+        DashBoardFragment fragment = new DashBoardFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dash_board, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
